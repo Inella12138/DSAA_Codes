b51385f527947cbdc22ab881771c1d11a4fc70d6
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-09-08 10:00:44 +0800

    Count String Length
    this workshop is weird and makes no sense

---
+++ Recursive_CountString/CountStrLen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_CountString
+{
+    public static class CountStrLen
+    {
+        public static int Count(string str, string impl = "recursive")
+        {
+            if (impl != "recursive" && impl != "iterative")
+            {
+                throw new Exception("Unknown implement type.");
+            }
+            return (impl == "recursive") ? RCount(str) : ICount(str);
+        }
+
+        private static int RCount(string str)
+        {
+            if (str == "")
+            {
+                return 0;
+            }
+            return 1 + RCount(str.Remove(0, 1));
+        }
+
+        private static int ICount(string str)
+        {
+            int cnt = 0;
+            while (str != "")
+            {
+                cnt++;
+                str = str.Remove(0, 1);
+            }
+            return cnt;
+        }
+    }
+}
---
+++ Recursive_CountString/Program.cs
@@ -0,0 +1,15 @@
+﻿using Recursive_CountString;
+
+string impl = "iterative";
+Reverse("hello world");
+Reverse("hello world", impl);
+Reverse("apple");
+Reverse("apple", impl);
+Reverse("toy");
+Reverse("toy", impl);
+Reverse("");
+Reverse("", impl);
+static void Reverse(string str, string impl = "recursive")
+{
+    Console.WriteLine("'{0}' has a length of '{1}'", str, CountStrLen.Count(str, impl));
+}
