16f7618639f670753a03cd4bde7654bcbdd31ef4
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-09-08 09:34:22 +0800

    ReverseString

---
+++ Recursion_ReverseString/Program.cs
@@ -0,0 +1,15 @@
+﻿using Recursion_ReverseString;
+
+string impl = "iterative"; 
+Reverse("hello world"); 
+Reverse("hello world", impl); 
+Reverse("apple");
+Reverse("apple", impl); 
+Reverse("toy"); 
+Reverse("toy", impl); 
+Reverse(""); 
+Reverse("", impl); 
+static void Reverse(string str, string impl = "recursive")
+{
+    Console.WriteLine("'{0}' reversed is '{1}'",str, ReverseString.Reverse(str, impl));
+}
---
+++ Recursion_ReverseString/ReverseString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion_ReverseString
+{
+    public static class ReverseString
+    {
+        public static string Reverse(string str, string impl = "recursive")
+        {
+            if (impl != "recursive" && impl != "iterative")
+            {
+                throw new Exception("Unknown implement type.");
+            }
+            if (str.Length == 0)
+            {
+                return "";
+            }
+            return (impl == "recursive") ? RReverse(str) : IReverse(str);
+        }
+
+
+        public static string RReverse(string str)
+        {
+            if (str == "") { return ""; }
+            return str.Substring(str.Length - 1, 1) + RReverse(str.Substring(0, str.Length - 1));
+        }
+
+        public static string IReverse(string str)
+        {
+            string rstr = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                rstr += str[i];
+            }
+            return rstr;
+        }
+    } 
+}
