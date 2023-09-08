4c0e12b8618089a1748101ca81816016b55e63a1
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-09-08 09:10:05 +0800

    Add Recursion Method

---
+++ DelimetersChecker/Checker.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelimetersChecker
+{
+    public class Checker
+    {
+        public static bool IsOpenDelimeter(char ch)
+        {
+            return ch == '(' || ch == '[' || ch == '{';
+        }
+
+        public static bool IsCloseDelimeter(char ch)
+        {
+            return ch == ')' || ch == ']' || ch == '}';
+        }
+
+        public static bool IsDelimeter(char ch)
+        {
+            return ch == '(' || ch == '[' || ch == '{' || ch == ')' || ch == ']' || ch == '}';
+        }
+
+        public static bool IsPaired(char open, char close)
+        {
+            return (open == '(' && close == ')') ||
+                   (open == '[' && close == ']') ||
+                   (open == '{' && close == '}');
+        }
+
+        public static bool CheckDelimeter(string input)
+        {
+            Stack<char> open = new();//为左括号建立一个栈
+            Queue<char> close = new();//为右括号建立一个队列
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (IsOpenDelimeter(input[i]))
+                {
+                    open.Push(input[i]);//检测到左括号，压栈
+                }
+                if (IsCloseDelimeter(input[i]))
+                {
+                    close.Enqueue(input[i]);//检测到右括号，加入队列
+                }
+            }
+            if (open.Count != close.Count)//数量不相等则一定不匹配
+            {
+                return false;
+            }
+            else
+            {
+                if (!IsPaired(open.Pop(), close.Dequeue()))
+                //为什么是用左括号退栈的值和右括号退出队列的值比较？
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //或许可以用递归的方法完成上述操作
+        public static bool RecursionChecker(string input)
+        {
+            string dms = "";
+            for (int i = 0; i< input.Length; i++)
+            {
+                if (IsDelimeter(input[i]))
+                {
+                    dms += input[i];
+                }
+            }
+            if ((dms.Length % 2) != 0)
+            {
+                return false;
+            }
+            else return IsDelimeterPaierd(dms);
+        }
+
+        public static bool IsDelimeterPaierd(string input)
+        {
+            if (input == "")
+            {
+                return true;
+            }
+            if (!IsPaired(input[0], input[input.Length - 1]))
+            {
+                return false;
+            }
+            else
+            {
+                string output = input.Substring(1, input.Length - 2);
+                return IsDelimeterPaierd(output);
+            }
+        }
+    }
+}
--- DelimetersChecker/Program.cs
+++ DelimetersChecker/Program.cs
@@ -1,53 +1,11 @@
-﻿string input1 = "a {b [c(d + e)/2 - f] +1}";
+﻿using DelimetersChecker;
+
+string input1 = "a {b [c(d + e)/2 - f] +1}";
 string input2 = "a {b [c (d + e)/2 - f  ] + 1";
 string input3 = "{a [b + (c + 2)/d ] + e) + f }";
-Console.WriteLine(CheckDelimeter(input1));
-Console.WriteLine(CheckDelimeter(input2));
-Console.WriteLine(CheckDelimeter(input3));
-
-static bool IsOpenDelimeter(char ch)
-{
-    return ch == '(' || ch == '[' || ch == '{';
-}
-
-static bool IsCloseDelimeter(char ch)
-{
-    return (ch == ')' || ch == ']' || ch == '}');
-}
-
-static bool IsPaired(char open, char close)
-{
-    return (open == '(' && close == ')') ||
-           (open == '[' && close == ']') ||
-           (open == '{' && close == '}');
-}
-
-bool CheckDelimeter(string input)
-{
-    Stack<char> open = new();//为左括号建立一个栈
-    Queue<char> close = new();//为右括号建立一个队列
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (IsOpenDelimeter(input[i]))
-        { 
-            open.Push(input[i]);//检测到左括号，压栈
-        }
-        if (IsCloseDelimeter(input[i]))
-        {
-            close.Enqueue(input[i]);//检测到右括号，加入队列
-        }
-    }
-    if (open.Count != close.Count)//数量不相等则一定不匹配
-    {
-        return false;
-    }
-    else
-    {
-        if (!IsPaired(open.Pop(), close.Dequeue()))
-            //为什么是用左括号退栈的值和右括号退出队列的值比较？
-        {
-            return false;
-        }
-    }
-    return true;
-}
+Console.WriteLine(Checker.CheckDelimeter(input1));
+Console.WriteLine(Checker.CheckDelimeter(input2));
+Console.WriteLine(Checker.CheckDelimeter(input3));
+Console.WriteLine(Checker.RecursionChecker(input1));
+Console.WriteLine(Checker.RecursionChecker(input2));
+Console.WriteLine(Checker.RecursionChecker(input3));
