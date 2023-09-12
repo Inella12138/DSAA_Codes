cdcd6789aea655dd425ad69bb4032c2a714b56ba
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-09-12 15:26:03 +0800

    staircase

---
+++ Staircase/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = Int32.Parse(Console.ReadLine());
+Console.WriteLine(PossibleChoices(n));
+
+static int PossibleChoices(int stairs)
+{
+    if (stairs == 0) { return 0; }
+    if (stairs == 1) { return 1; }
+    if (stairs == 2) { return 2; }
+    if (stairs == 3) { return 4; }
+    return PossibleChoices(stairs - 1) + PossibleChoices(stairs - 2) + PossibleChoices(stairs - 3);
+}
